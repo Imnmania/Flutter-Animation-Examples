be3a198fbed02da4d949ea5f31846505910f204e
Imnmania <niloyb@live.com>
2023-03-06 21:03:32 +0600

    example 7 complete

--- animations_example_7/lib/home_screen.dart
+++ animations_example_7/lib/home_screen.dart
@@ -2,20 +2,103 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late final AnimationController _sidesController;
+  late final Animation<int> _sidesAnimation;
+
+  late final AnimationController _radiusController;
+  late final Animation<double> _radiusAnimation;
+
+  late final AnimationController _rotationController;
+  late final Animation<double> _rotationAnimation;
+
+  @override
+  void initState() {
+    _sidesController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _sidesAnimation = IntTween(
+      begin: 3,
+      end: 10,
+    ).animate(_sidesController);
+
+    _radiusController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _radiusAnimation = Tween(
+      begin: 20.0,
+      end: 400.0,
+    )
+        .chain(
+          CurveTween(curve: Curves.bounceInOut),
+        )
+        .animate(_radiusController);
+
+    _rotationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _rotationAnimation = Tween(begin: 0.0, end: 2 * pi)
+        .chain(CurveTween(curve: Curves.easeInOut))
+        .animate(_rotationController);
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _sidesController.dispose();
+    _radiusController.dispose();
+    _rotationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _sidesController.repeat(reverse: true);
+    _radiusController.repeat(reverse: true);
+    _rotationController.repeat(reverse: true);
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: CustomPaint(
-          painter: Polygon(sides: 10),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.width,
-            width: MediaQuery.of(context).size.width,
-          ),
-        ),
+        child: AnimatedBuilder(
+            animation: Listenable.merge([
+              _sidesController,
+              _radiusController,
+              _rotationController,
+            ]),
+            builder: (context, child) {
+              return Transform(
+                alignment: Alignment.center,
+                transform: Matrix4.identity()
+                  ..rotateX(_rotationAnimation.value)
+                  ..rotateY(_rotationAnimation.value)
+                  ..rotateZ(_rotationAnimation.value),
+                child: CustomPaint(
+                  painter: Polygon(sides: _sidesAnimation.value),
+                  child: SizedBox(
+                    height: _radiusAnimation.value,
+                    width: _radiusAnimation.value,
+                  ),
+                ),
+              );
+            }),
       ),
     );
   }
